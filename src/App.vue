e440d9c9e05d01a480f2c1a11d776d8607aa8042
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-10-14 17:51:22 +0200

    [UPD] created new component ProjectCard

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script>
 import axios from "axios"
+import ProjectCard from "./components/ProjectCard.vue"
 
 export default {
+    components: {
+        ProjectCard,
+    },
+    data() {
+        return {
+            projects: [],
+        }
+    },
     methods: {
         getProjects() {
             axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
                 console.log(response.data)
+                this.projects = response.data
             })
         },
     },
@@ -15,6 +25,8 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <ProjectCard />
+</template>
 
 <style scoped></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,9 @@
+<script>
+export default {}
+</script>
+
+<template>
+    <div>ciao</div>
+</template>
+
+<style scoped></style>
