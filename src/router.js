a5da57a6b50080eab42f80afae67ff7611720bbf
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-10-15 14:44:19 +0200

    [UPD] created router.js and two component: HomePage and Portfolio

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from "vue-router"
+
+import Home from "./components/HomePage.vue"
+import Portfolio from "./components/Portfolio.vue"
+
+const routes = [
+    { path: "/", component: Home },
+    { path: "/portfolio", component: Portfolio },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router
